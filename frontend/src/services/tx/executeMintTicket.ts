0fbfc29d203e3b7bc8b46c1d212f7d382fe46842
Hoàng_Hữu_Nghĩa <fishhh0978@gmail.com>
2026-05-07 22:42:41 +0700

    luu

--- frontend/src/services/tx/executeMintTicket.ts
+++ frontend/src/services/tx/executeMintTicket.ts
@@ -4,13 +4,12 @@ export const handleMintTicket = async (movieName: string, quantity: number) => {
   const txb = new Transaction();
 
   // Thay địa chỉ này bằng Package ID bạn đã deploy lên GitHub
-  const PACKAGE_ID = "0x9247b3046f1c72afd95e78fa9d76fe73570ae6cdfb0827a04c92c99a7db74235";
+  const PACKAGE_ID = "0xfb9572bdbac6c94edce9c547a476e2a9a4d9b044e13b30813d7e24e650b2cfaa";
   // Ví nhận tiền của Mạnh
   const ADMIN_ADDRESS = "0x17d3394e754b09fd264781e9c029d7d19578c4ac43fd6c47b8dd3cec00a6fee5";
 
   // 1. Tách đúng 5 SUI từ số dư hiện có của người dùng
-  const [paymentCoin] = txb.splitCoins(txb.gas, [txb.pure.u64(5000000000)]);
-
+  const [paymentCoin] = txb.splitCoins(txb.gas, [txb.pure.u64(5000000000 * quantity)]);
   // 2. Thực hiện gọi hàm Move
   txb.moveCall({
     target: `${PACKAGE_ID}::suiticket::mint_ticket`,
